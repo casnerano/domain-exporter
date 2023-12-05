11a5b537b595e1581349d039cf0ad51d23ddb392
Arutyunyan A <a.arutyunyan@tanais.ru>
2023-12-05 17:37:00 +0300

    addee stub endpoints

--- domain-exporter.go
+++ domain-exporter.go
@@ -3,11 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type domain struct {
@@ -24,7 +27,19 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
+		if err := getIndexTemplate().Execute(w, nil); err != nil {
+			log.Println("Failed render index page.")
+		}
+	})
+
+	mux.Handle("/metrics", promhttp.Handler())
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		target := strings.TrimSpace(r.URL.Query().Get("target"))
+		if target == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			// ......
+		}
 	})
 
 	if err := http.ListenAndServe(*addr, mux); err != nil {
@@ -66,4 +81,21 @@ func whois(host string) *domain {
 	}
 
 	return &d
+}
+
+func getIndexTemplate() *template.Template {
+	return template.Must(
+		template.New("index").Parse(
+			`<html>
+				<head>
+					<title>Domain Exporter</title>
+				</head>
+				<body>
+					<h1>Domain Exporter</h1>
+					<p><a href="/metrics"></a></p>
+					<p><a href="/probe?target=ya.ru">probe ya.ru</a></p>
+				</body>
+			</html>`,
+		),
+	)
 }
